f200768c9da339788c0527b6b221583691cd3610
Robert Paulson <xantelfy@gmail.com>
2025-08-15 12:47:32 +0300

    Create TopicSeeder.php

---
+++ database/seeders/TopicSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Topic;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TopicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $topics = [
+            ['id' => 1, 'title' => 'php'],
+            ['id' => 2, 'title' => 'laravel'],
+            ['id' => 3, 'title' => 'sql'],
+            ['id' => 3, 'title' => 'mixed'],
+        ];
+        foreach ($topics as $topic) {
+            Topic::updateOrCreate(['id' => $topic['id']], $topic);
+        }
+    }
+}
+
